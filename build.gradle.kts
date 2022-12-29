d5c6784ee8429ca07c81544ad90480ee3eec8aed
Todd Harrison <todd@eharrison.com>
2022-12-29 15:40:03 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,17 +66,17 @@ subprojects {
 
     configure<PublishingExtension> {
         repositories {
-            maven {
-                url = uri("$buildDir/local-repository")
-            }
-//            maven {
-//                name = "GitHubPackages"
-//                url = uri("https://maven.pkg.github.com/toddharrison/BriarCode")
-//                credentials {
-//                    username = System.getenv("GITHUB_ACTOR")
-//                    password = System.getenv("GITHUB_TOKEN")
-//                }
-//            }
+//             maven {
+//                 url = uri("$buildDir/local-repository")
+//             }
+           maven {
+               name = "GitHubPackages"
+               url = uri("https://maven.pkg.github.com/toddharrison/BriarCode")
+               credentials {
+                   username = System.getenv("GITHUB_ACTOR")
+                   password = System.getenv("GITHUB_TOKEN")
+               }
+           }
         }
     }
 }
