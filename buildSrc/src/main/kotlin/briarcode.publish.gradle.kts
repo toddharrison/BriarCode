077ab9d02243e0d2ea96e57a99ba66f4adba2121
Todd Harrison <todd@eharrison.com>
2023-02-09 20:58:00 -0700

    Working on jitpack.

--- buildSrc/src/main/kotlin/briarcode.publish.gradle.kts
+++ buildSrc/src/main/kotlin/briarcode.publish.gradle.kts
@@ -4,23 +4,33 @@ plugins {
     `maven-publish`
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["java"])
+            }
         }
-    }
-    repositories {
-        maven {
-            url = uri("$buildDir/local-repository")
-        }
-//        maven {
-//            name = "GitHubPackages"
-//            url = uri("https://maven.pkg.github.com/toddharrison/BriarCode")
-//            credentials {
-//                username = System.getenv("GITHUB_ACTOR")
-//                password = System.getenv("GITHUB_TOKEN")
-//            }
-//        }
     }
 }
+
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            from(components["java"])
+//        }
+//    }
+//    repositories {
+//        maven {
+//            url = uri("$buildDir/local-repository")
+//        }
+////        maven {
+////            name = "GitHubPackages"
+////            url = uri("https://maven.pkg.github.com/toddharrison/BriarCode")
+////            credentials {
+////                username = System.getenv("GITHUB_ACTOR")
+////                password = System.getenv("GITHUB_TOKEN")
+////            }
+////        }
+//    }
+//}
