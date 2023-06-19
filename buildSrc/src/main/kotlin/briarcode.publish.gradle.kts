f19f914e61c7c83a2b0a2a919bd85121fa9491a6
Todd Harrison <todd@eharrison.com>
2023-06-19 12:26:37 -0600

    Fixed uri for BriarCode publish.

--- buildSrc/src/main/kotlin/briarcode.publish.gradle.kts
+++ buildSrc/src/main/kotlin/briarcode.publish.gradle.kts
@@ -15,7 +15,7 @@ publishing {
 //        }
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/toddharrison/Demo")
+            url = uri("https://maven.pkg.github.com/toddharrison/BriarCode")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
